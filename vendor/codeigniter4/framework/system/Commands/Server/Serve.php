ccb0a7efeaa50eb1dc9984723edd8ec2d9638da4
ppannji29 <ppamungkas29@gmail.com>
2022-12-08 15:08:07 +0700

    [Commit] Update PRDI Notif

--- vendor/codeigniter4/framework/system/Commands/Server/Serve.php
+++ vendor/codeigniter4/framework/system/Commands/Server/Serve.php
@@ -79,8 +79,10 @@ class Serve extends BaseCommand
      */
     protected $options = [
         '--php'  => 'The PHP Binary [default: "PHP_BINARY"]',
-        '--host' => 'The HTTP Host [default: "localhost"]',
-        '--port' => 'The HTTP Host Port [default: "8080"]',
+        // '--host' => 'The HTTP Host [default: "localhost"]',
+        '--host' => 'The HTTP Host [default: "172.251.38.62"]',
+        '--port' => 'The HTTP Host Port [default: "80"]',
+        // '--port' => 'The HTTP Host Port [default: "8080"]',
     ];
 
     /**
@@ -90,8 +92,10 @@ class Serve extends BaseCommand
     {
         // Collect any user-supplied options and apply them.
         $php  = escapeshellarg(CLI::getOption('php') ?? PHP_BINARY);
-        $host = CLI::getOption('host') ?? 'localhost';
-        $port = (int) (CLI::getOption('port') ?? 8080) + $this->portOffset;
+        // $host = CLI::getOption('host') ?? 'localhost';
+        $host = CLI::getOption('host') ?? '172.251.38.62';
+        // $port = (int) (CLI::getOption('port') ?? 8080) + $this->portOffset;
+        $port = (int) (CLI::getOption('port') ?? 80) + $this->portOffset;
 
         // Get the party started.
         CLI::write('CodeIgniter development server started on http://' . $host . ':' . $port, 'green');
